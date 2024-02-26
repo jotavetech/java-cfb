f78aeba65bb3a9d876664f618cac7ad27e300a28
jotavetech <ojoaogugel@gmail.com>
2024-02-25 22:08:44 -0300

    classes 06

---
+++ Classes02/src/Aviao.java
@@ -0,0 +1,11 @@
+public class Aviao extends Veiculo {
+    private int categoria;
+
+    public Aviao(String nome, int tipo, int categoria) {
+        super(nome, tipo);
+        this.categoria = categoria;
+    }
+
+
+
+}
